67333f61c815bd98c999d6bb427655436868d606
Rustam <tabarzin@gmail.com>
2024-05-29 19:00:38 +0300

    refactor

--- filter/script.js
+++ filter/script.js
@@ -5,8 +5,8 @@ function myFunction() {
   const li = ul.getElementsByTagName("li");
 
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+    const a = li[i].getElementsByTagName("a")[0];
+    const txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
