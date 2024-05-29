15bfe4bb231ba70b459579ad6aaaae2e9a0edb87
Rustam <tabarzin@gmail.com>
2024-05-29 15:32:58 +0300

    feat: add form

---
+++ form/script.js
@@ -0,0 +1,73 @@
+const myInput = document.getElementById("psw");
+const letter = document.getElementById("letter");
+const capital = document.getElementById("capital");
+const number = document.getElementById("number");
+const length = document.getElementById("length");
+const form = document.getElementById("form");
+const success = document.getElementById("successMessage");
+const submitBtn = document.getElementById("submitBtn");
+
+myInput.onfocus = function () {
+  document.getElementById("message").style.display = "block";
+};
+
+myInput.onblur = function () {
+  document.getElementById("message").style.display = "none";
+};
+
+myInput.onkeyup = function () {
+  const lowerCaseLetters = /[a-z]/g;
+  if (myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+
+  const upperCaseLetters = /[A-Z]/g;
+  if (myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  const numbers = /[0-9]/g;
+  if (myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  if (myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+  toggleSubmitButton();
+};
+
+function toggleSubmitButton() {
+  if (
+    letter.classList.contains("valid") &&
+    capital.classList.contains("valid") &&
+    number.classList.contains("valid") &&
+    length.classList.contains("valid")
+  ) {
+    submitBtn.classList.remove("hidden");
+  } else {
+    submitBtn.classList.add("hidden");
+  }
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  success.style.display = "block";
+});
