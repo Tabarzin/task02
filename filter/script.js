43cf186d6b1e16394c1006547d243da90574b2d7
Rustam <tabarzin@gmail.com>
2024-05-29 18:47:21 +0300

    refactor

--- filter/script.js
+++ filter/script.js
@@ -1,12 +1,9 @@
 function myFunction() {
-  // Declare variables
-
   const input = document.getElementById("myInput");
   const filter = input.value.toUpperCase();
   const ul = document.getElementById("myUL");
   const li = ul.getElementsByTagName("li");
 
-  // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
