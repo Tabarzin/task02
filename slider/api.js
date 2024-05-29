76d30ce710441abea0adb30728f42930c9a94929
Rustam <tabarzin@gmail.com>
2024-05-29 19:00:05 +0300

    refactor

--- slider/api.js
+++ slider/api.js
@@ -1,13 +1,33 @@
+// export async function fetchPhotos() {
+//   try {
+//     const response = await axios.get(
+//       "https://jsonplaceholder.typicode.com/photos"
+//     );
+//     const photos = response.data;
+//     const photoDetails = photos.map((photo) => {
+//       return {
+//         title: photo.title,
+//         url: photo.thumbnailUrl,
+//       };
+//     });
+//     return photoDetails;
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return [];
+//   }
+// }
+
 export async function fetchPhotos() {
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/photos"
     );
     const photos = response.data;
-    const photoDetails = photos.map((photo) => {
+    const limitedPhotos = photos.slice(0, 20);
+    const photoDetails = limitedPhotos.map((photo) => {
       return {
         title: photo.title,
-        url: photo.thumbnailUrl,
+        url: photo.url,
       };
     });
     return photoDetails;
